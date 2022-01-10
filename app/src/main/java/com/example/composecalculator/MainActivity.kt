1feae30b8ec5f535df6ffe495f22f57fe179e043
BeniwasabiKarashi <karashibeniwasabi@gmail.com>
2022-01-11 04:35:38 +0900

    [fix]Remove unused import

--- app/src/main/java/com/example/composecalculator/MainActivity.kt
+++ app/src/main/java/com/example/composecalculator/MainActivity.kt
@@ -6,13 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.composecalculator.data.Calculate
 import com.example.composecalculator.ui.theme.ComposeCalculatorTheme
 
